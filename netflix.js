6ca0063e6a33b40112aa11caec4124cf32aabdd4
Shammasahamedp <159105756+Shammasahamedp@users.noreply.github.com>
2024-02-15 15:29:12 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,54 @@
+function show1(){
+     var show=document.getElementById('answer1');
+     if(show.style.display=='none'){
+        show.style.display='block'
+     }
+     else{
+        show.style.display='none'
+     }
+}
+function show2(){
+    var show=document.getElementById('answer2');
+    if(show.style.display=='none'){
+        show.style.display='block'
+     }
+     else{
+        show.style.display='none'
+     }
+}
+function show3(){
+    var show=document.getElementById('answer3');
+    if(show.style.display=='none'){
+        show.style.display='block'
+     }
+     else{
+        show.style.display='none'
+     }
+}
+function show4(){
+    var show=document.getElementById('answer4');
+    if(show.style.display=='none'){
+        show.style.display='block'
+     }
+     else{
+        show.style.display='none'
+     }
+}
+function show5(){
+    var show=document.getElementById('answer5');
+    if(show.style.display=='none'){
+        show.style.display='block'
+     }
+     else{
+        show.style.display='none'
+     }
+}
+function show6(){
+    var show=document.getElementById('answer6');
+    if(show.style.display=='none'){
+        show.style.display='block'
+     }
+     else{
+        show.style.display='none'
+     }
+}
